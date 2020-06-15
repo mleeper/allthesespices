f043e3e00e8de80cf72fa58ff1833c928693abf9
Marcus <marcus@Marcuss-MacBook-Air.local>
2020-06-15 16:50:09 -0700

    fix: add single root to articles page.

--- components/ArticleExcerptList.vue
+++ components/ArticleExcerptList.vue
@@ -13,17 +13,20 @@
         },
         render(h) {
             return (
-                this.list.map(article => (
-                    <ArticleExcerpt
-                        key={article.title}
-                        name={article.name}
-                        date={article.date}
-                        title={article.title}
-                        category={article.category}
-                        text={article.excerpt}
-                        image={article.image}
-                    />
-                ))
+                <div>
+                    {this.list.map(article => (
+                        <ArticleExcerpt
+                            key={article.title}
+                            name={article.name}
+                            date={article.date}
+                            title={article.title}
+                            category={article.category}
+                            text={article.excerpt}
+                            image={article.image}
+                        />
+                        ))
+                    }
+                </div>
             );
         }
     }
